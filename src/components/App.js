f281df072c22369357be594668065a4ac1064aaa
0surface <tms.0xvoid@gmail.com>
2020-10-17 13:20:44 +0100

    Added,rendered courses page

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
-    return route === "/about" ? <AboutPage /> : <HomePage />;
+    if (route === "/") return <HomePage />;
+    if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursesPage />;
   }
   return (
     <div className="container-Fluid">
