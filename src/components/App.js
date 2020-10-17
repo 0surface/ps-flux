a695ac9b8c93b0c6b6226f20ccedce6551461638
0surface <tms.0xvoid@gmail.com>
2020-10-17 17:17:53 +0100

    Add Route,Link, NavLink,404 page

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,19 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
+import { Route, Switch } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/") return <HomePage />;
-    if (route === "/about") return <AboutPage />;
-    if (route === "/courses") return <CoursesPage />;
-  }
   return (
     <div className="container-Fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
